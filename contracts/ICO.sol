08eb7d6e9b06081bf8e753e172d926c04470dfe2
Zainab Oufqir <zayna.oufkir@gmail.com>
2022-10-07 21:24:43 +0100

    define state variables and constructor

--- contracts/ICO.sol
+++ contracts/ICO.sol
@@ -5,4 +5,23 @@ import "./Token.sol";
 
 contract ICO is Token{
 
+    address public admin;
+    address payable public deposit; 
+    uint public constant tokenPrice = 0.001 ether; // token price
+    uint public raisedAmount;
+    uint public constant maxCap = 300 ether;
+    uint public saleStart = block.timestamp;
+    uint public saleEnd = block.timestamp + 604800; // ico ends after 1 week
+    uint public tradeStart = saleEnd + 604800; // lock tokens for one week
+    uint public constant maxInvest = 5 ether;
+    uint public constant minInvest = 0.01 ether;
+    enum State { beforeStart, running, afterEnd, halted }
+    State public icoState; 
+
+    constructor(address payable _deposit){
+        deposit = _deposit;
+        admin = msg.sender;
+        icoState = State.beforeStart;
+    }
+
 }
